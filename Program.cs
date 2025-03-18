2e15e8549770a10f29080130a41db8b64aed0330
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-03-18 14:17:20 -0600

    map

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿// Chandler Wixom, 3/18/2025, Lab 7 Maze
 
-Console.WriteLine("This is a maze game\nUse (↑ ← ↓ →) or (w a s d) to move");
+Console.WriteLine("This is a maze game\nUse (up, down, left, right) or (w, a, s, d) to move");
+
+string[] mapRows = File.ReadAllLines("..\\..\\..\\map.txt");
+
+Console.Clear();
+
+foreach (var line in mapRows)
+{
+    Console.WriteLine(line);
+}
+
