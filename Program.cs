42d295ae69027c986f2364a7fd0d1bec16b025db
Chandler Wixom <chandlerj.wixom@gmail.com>
2025-03-18 14:42:36 -0600

    basic Movement

--- Program.cs
+++ Program.cs
@@ -11,3 +11,50 @@ foreach (var line in mapRows)
     Console.WriteLine(line);
 }
 
+
+Console.SetCursorPosition(0,0);
+
+do
+{
+    WaitKey();
+}
+while(WaitKey() != 6);
+
+
+
+
+
+
+int WaitKey()
+{
+   var key = Console.ReadKey(true).Key;
+   if (key == ConsoleKey.Escape)
+   {
+     return 6;
+   }
+   else if (key == ConsoleKey.UpArrow || key == ConsoleKey.W)
+    {
+        Console.CursorTop--;
+        return 1;
+    }
+    else if (key == ConsoleKey.DownArrow || key == ConsoleKey.S)
+    {
+        Console.CursorTop++;
+        return 2;
+    }
+    else if (key == ConsoleKey.RightArrow || key == ConsoleKey.D)
+    {
+        Console.CursorLeft++;
+        return 3;
+    }
+    else if (key == ConsoleKey.LeftArrow || key == ConsoleKey.A)
+    {
+        Console.CursorLeft--;
+        return 4;
+    }
+    else 
+    {
+        return 5;
+    }
+}
+
